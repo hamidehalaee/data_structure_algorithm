e483df497500eeaeead2e5843c4ceb9567f7aa61
hamidealaee <hamidealaeeyasar@gmail.com>
2021-08-10 11:17:19 -0700

    O(n)

---
+++ 1.bigOnotation/README.md
@@ -0,0 +1,3 @@
+Big O is a way of describing the efficiency of algorithms without getting too mired in the details. It describes how the time (or the number of operations needed) it takes to run grows as the size of the input grows.
+
+Big O notation helps us answer the question, "How do our functions or algorithms behave/scale when the size of the inputs increases significantly
---
+++ 1.bigOnotation/bigOnotation.js
@@ -0,0 +1,12 @@
+// Big O is a way of describing the efficiency of 
+// algorithms without getting too mired in the details. 
+// It describes how the time (or the number of operations needed)
+// it takes to run grows as the size of the input grows.
+const colors = require('../console.colorizing')
+
+console.time(colors.FgGreen, 'test');
+
+let array = [1,2,3,4]
+for(let i = 0 ; i < array.length; i++) console.log(colors.cyan,array[i]) 
+
+console.timeEnd(colors.FgGreen, 'test')
