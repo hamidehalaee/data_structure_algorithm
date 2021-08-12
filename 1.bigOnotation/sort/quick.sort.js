122558eca0166a8d660c0841f4d5044dafac1efa
hamidealaee <hamidealaeeyasar@gmail.com>
2021-08-12 10:31:24 -0700

    memory usage[quickSort]

--- 1.bigOnotation/sort/quick.sort.js
+++ 1.bigOnotation/sort/quick.sort.js
@@ -1,21 +1,16 @@
 
 const colors = require('../../console.colorizing')
-
-
-
 console.time(colors.FgGreen, 'test');
 
 function quick_Sort(origArray) {
 	if (origArray.length <= 1) { 
 		return origArray;
 	} else {
-
 		var left = [];
 		var right = [];
 		var newArray = [];
 		var pivot = origArray.pop();
 		var length = origArray.length;
-
 		for (var i = 0; i < length; i++) {
 			if (origArray[i] <= pivot) {
 				left.push(origArray[i]);
@@ -23,18 +18,17 @@ function quick_Sort(origArray) {
 				right.push(origArray[i]);
 			}
 		}
-
 		return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
 	}
 }
 
-var myArray = [1,3,2,5,9,8,5,2]//14.29ms
-var foo = Array.from(Array(Math.floor(Math.random() * 10000000)).keys())//<--- Last few GCs --->
-const a = require('./create')//2.901s
-quick_Sort(a);
-// console.log("Sorted array: " + sortedArray);
+var myArray = [1,3,2,5,9,8,5,2],//14.29ms
+oneMillionItems = require('./create');//2.901s
+quick_Sort(oneMillionItems);
+
 
 console.timeEnd(colors.FgGreen, 'test') 
+
 const used = process.memoryUsage();
 for (let key in used) {
   console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
