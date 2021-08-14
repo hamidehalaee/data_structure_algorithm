497d0eda3b14d7fd61b0b33445f7844dfef8fa74
hamidealaee <hamidealaeeyasar@gmail.com>
2021-08-13 22:33:36 -0700

    time & space usage[binarySearch]

--- 1.bigOnotation/search/binary.search.js
+++ 1.bigOnotation/search/binary.search.js
@@ -24,11 +24,11 @@ let recursiveFunction = function (arr, x, start, end) {
 }
   
 // Driver code
-let arr = [1, 3, 5, 7, 8, 9];
-const oneMillionItems = require('./../sort/create')//133.31ms
+let arr = [1, 3, 5, 7, 8, 9];//13.383ms
+// const oneMillionItems = require('./../sort/create')//133.31ms
 let x = 223;
   
-if (recursiveFunction(oneMillionItems, x, 0, oneMillionItems.length-1)){
+if (recursiveFunction(arr, x, 0, arr.length-1)){
     console.log('find')
 }else{
     console.log('not found')
@@ -40,3 +40,19 @@ const used = process.memoryUsage();
 for (let key in used) {
   console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
 }
+
+// oneMillionItems => {
+//     rss 53.84 MB
+//     heapTotal 51.91 MB
+//     heapUsed 34.07 MB
+//     external 1.02 MB
+//     arrayBuffers 0.01 MB
+// }
+
+// arr =>{
+//     rss 18.79 MB
+//     heapTotal 4.77 MB
+//     heapUsed 3.06 MB
+//     external 1.03 MB
+//     arrayBuffers 0.01 MB
+// }
